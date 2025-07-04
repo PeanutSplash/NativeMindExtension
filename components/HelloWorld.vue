585d527e7c7423781853e0932e6fa5aa6ee7d3d6
PeanutSplash <b1300658700@outlook.com>
2025-07-04 10:43:35 +0800

    修复 ESLint 和 TypeScript 错误

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="p-4 text-center">
-    <h1 class="text-xl font-bold">{{ msg }}</h1>
-    <button class="mt-2 px-4 py-1 bg-blue-500 text-white rounded" @click="count++">
+    <h1 class="text-xl font-bold">
+      {{ msg }}
+    </h1>
+    <button
+      class="mt-2 px-4 py-1 bg-blue-500 text-white rounded"
+      @click="count++"
+    >
       You clicked {{ count }} times
     </button>
   </div>
@@ -10,6 +15,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const props = defineProps<{ msg: string }>()
+defineProps<{ msg: string }>()
 const count = ref(0)
 </script>
--- entrypoints/content/index.ts
+++ entrypoints/content/index.ts
@@ -1,1 +1,8 @@
-// content script entrypoint
+import { defineContentScript } from 'wxt/utils/define-content-script'
+export default defineContentScript({
+  matches: ['<all_urls>'],
+  main() {
+    // eslint-disable-next-line no-console
+    console.log('Hello content script!')
+  },
+})
